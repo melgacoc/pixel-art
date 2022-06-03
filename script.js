00ce6ec40f342aac35c9be292aa587f400c0fd6f
Claudio Melgaco <claudio19a@hotmail.com>
2022-06-03 16:07:20 -0300

    .

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-window.onload = function(){
-    const pixelColor = document.querySelectorAll('.pixel');
-    const container = document.getElementsById('line');
-    const creatButton = document.getElementById('clear-board');
-
+//window.onload = function(){
+   // const pixelColor = document.querySelectorAll('.pixel');
+    //const container = document.getElementsById('line');
+    //const creatButton = document.getElementById('clear-board');
 
     //creatButton.addEventListener('click', function(pixelColor)){
     //    pixelColor.style.backgroundColor = 'white';
    // }
-
-    function clear(pixelColor){
-     for(index = 0; index < pixelColor.length; index += 1){
+    function clear(pixelColor) {
+     // eslint-disable-next-line no-undef
+     for (index = 0; index < pixelColor.length; index += 1){
         pixelColor[index].style.backgroundColor = 'white'; 
      }
-
-     function clearButton(){
-         creatButton.addEventListener('click',clear);
+    }
+    function clearButton() {
+        const creatButton = document.getElementById('clear-board');
+        creatButton.addEventListener('click',clear);
      }
 
-    }
-}
+    //}
+//}
