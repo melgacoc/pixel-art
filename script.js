c9c687a107d6f8e9c39fd43c635f35ef3a055ba8
Claudio Melgaco <claudio19a@hotmail.com>
2022-06-03 15:34:39 -0300

    Botão limpar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let pallet = document.getElementById('color-pallete');
+window.onload = function(){
+    const pixelColor = document.querySelectorAll('.pixel');
+    const container = document.getElementsById('line');
+    const creatButton = document.getElementById('clear-board');
+
+
+    //creatButton.addEventListener('click', function(pixelColor)){
+    //    pixelColor.style.backgroundColor = 'white';
+   // }
+
+    function clear(pixelColor){
+     for(index = 0; index < pixelColor.length; index += 1){
+        pixelColor[index].style.backgroundColor = 'white'; 
+     }
+
+     function clearButton(){
+         creatButton.addEventListener('click',clear);
+     }
+
+    }
+}
