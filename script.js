e98bdc0ec797639b4152a1d42128cefac7a74830
Claudio Melgaco <claudio19a@hotmail.com>
2022-06-04 16:23:02 -0300

    5 refeito em js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-//window.onload = function(){
-   // const pixelColor = document.querySelectorAll('.pixel');
-    //const container = document.getElementsById('line');
-    //const creatButton = document.getElementById('clear-board');
-
-    //creatButton.addEventListener('click', function(pixelColor)){
-    //    pixelColor.style.backgroundColor = 'white';
-   // }
+window.onload = function() {
     function clear(pixelColor) {
-     // eslint-disable-next-line no-undef
-     for (index = 0; index < pixelColor.length; index += 1){
-        pixelColor[index].style.backgroundColor = 'white'; 
-     }
+     for (index = 0; index < pixelColor.length; index += 1) {
+     // eslint-disable-next-line no-param-reassign
+     pixelColor[index].style.backgroundColor = 'white';
+   }
     }
-    function clearButton() {
-        const creatButton = document.getElementById('clear-board');
-        creatButton.addEventListener('click',clear);
-     }
+   // criando pixel
+    let container = document.getElementById('pixel-board');
+    function createPixel(classe){
+            let newDiv = document.createElement('div');
+            newDiv.className = classe;
+            return newDiv;
+    };
+    function createPixels(number){
+        for(index = 0; index < number; index +=1){
+            function addElementAsChild(local,elemento){
+                local.appendChild(elemento);
+            }
+            addElementAsChild(container,createPixel('pixel'));
+        }
+    }
+    createPixels(25);
 
-    //}
-//}
+}
