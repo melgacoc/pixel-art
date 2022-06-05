cc161e8e51791e7337471629283574177b143432
Claudio Melgaco <claudio19a@hotmail.com>
2022-06-05 16:17:05 -0300

    tentativa 7

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // eslint-disable-next-line max-lines-per-function
-window.onload = function() {
-     function clear(pixelColor) {
-         for (index = 0; index < pixelColor.length; index += 1) {
-         pixelColor[index].style.backgroundColor = 'white';
-         }
-     }
-   // criando pixel
-     let container = document.getElementById('pixel-board');
-         function createPixel(classe){
-         let newDiv = document.createElement('div');
-         newDiv.className = classe;
-         return newDiv;
-         };
-     function createPixels(number){
-         for(index = 0; index < number; index +=1){
-         function addElementAsChild(local,elemento){
-             local.appendChild(elemento);
+window.onload = function () {
+    function clear(pixelColor) {
+        for (index = 0; index < pixelColor.length; index += 1) {
+            pixelColor[index].style.backgroundColor = 'white';
+        }
+    }
+    // criando pixel
+    let container = document.getElementById('pixel-board');
+    function createPixel(classe) {
+        let newDiv = document.createElement('div');
+        newDiv.className = classe;
+        return newDiv;
+    };
+    function createPixels(number) {
+        for (index = 0; index < number; index += 1) {
+            function addElementAsChild(local, elemento) {
+                local.appendChild(elemento);
             }
-         addElementAsChild(container,createPixel('pixel'));
-         }
-     }
-     createPixels(25);
+            addElementAsChild(container, createPixel('pixel'));
+        }
+    }
+    createPixels(25);
 
-    //
-     
+    //escolhendo cor da paleta
+    const collor = document.getElementsByClassName('color');
+    const collorselect = document.querySelector('.selected');
 
-//botão de limpar
-     const button = document.getElementById('clear-board');
-     const pixelP = document.querySelectorAll('.pixel');
-     button.onclick = function() {
-         for (let index = 0; index < pixelP.length; index += 1) {
-             const pixelC = pixelP[index];
-             pixelC.style.backgroundColor = 'white';
+    function collorSelected(event){
+        collorselect.className.remove(collorselect);
+        event.target.className.add(collorselect);
+    }
+
+
+    //botão de limpar
+    const button = document.getElementById('clear-board');
+    const pixelBackground = document.querySelectorAll('.pixel');
+    button.addEventListener('click', clearBoard);
+    function clearBoard () {
+        for (let index = 0; index < pixelBackground.length; index += 1) {
+            const pixelColor = pixelBackground[index];
+            pixelColor.style.backgroundColor = 'white';
         }
     }
 
